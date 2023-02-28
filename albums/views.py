a6171eb8986d2715e71f11c144747bec0e8eb06b
netomilfont <netomilfont@hotmail.com>
2023-02-28 13:16:09 -0300

    correções da entrega

--- albums/views.py
+++ albums/views.py
@@ -16,13 +16,13 @@ class AlbumView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         return serializer.save(user_id=self.request.user.id)
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
+    # def list(self, request, *args, **kwargs):
+    #     queryset = self.filter_queryset(self.get_queryset())
 
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+    #     page = self.paginate_queryset(queryset)
+    #     if page is not None:
+    #         serializer = self.get_serializer(page, many=True)
+    #         return self.get_paginated_response(serializer.data)
 
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    #     serializer = self.get_serializer(queryset, many=True)
+    #     return Response(serializer.data)
